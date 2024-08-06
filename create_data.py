fa7baa9e668a3ac88251799b561cb6f7dfee05ba
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2024-08-06 12:50:09 +0200

    Add files via upload

---
+++ create_data.py
@@ -0,0 +1,33 @@
+from capp import db
+db.create_all()
+
+# from capp.models import Sentence
+# # Frase 1: Genero, Persona
+# incorrect_sentence = "Mi perro es bonita"
+# correct_sentence = "Mi perro es bonito"
+# your_sentence = "none"
+# objective = "Genero"
+# source = "Persona"
+# result_string = "none"
+# result_num= "nan"
+# group = 'general'
+# university = 'University of Salamanca'
+# year = 2024
+# sentence = Sentence(incorrect_sentence=incorrect_sentence, correct_sentence=correct_sentence, your_sentence=your_sentence, objective=objective, source=source, result_string=result_string, result_num=result_num,  group=group, university=university, year=year, author=current_user)
+# db.session.add(sentence)
+# db.session.commit()
+
+# # Frase 1: Numero, Persona
+# incorrect_sentence = "Mi perro es bonitos"
+# correct_sentence = "Mi perro es bonito"
+# your_sentence = "none"
+# objective = "Numero"
+# source = "Persona"
+# result_string = "none"
+# result_num= "nan"
+# group = 'general'
+# university = 'University of Salamanca'
+# year = 2024
+# sentence = Sentence(incorrect_sentence=incorrect_sentence, correct_sentence=correct_sentence, your_sentence=your_sentence, objective=objective, source=source, result_string=result_string, result_num=result_num,  group=group, university=university, year=year, author=current_user)
+# db.session.add(sentence)
+# db.session.commit()
